ab4730e853a90fc40dd496dbfba2357bc8c46a81
KOSASIH <kosasihg88@gmail.com>
2024-11-19 22:03:11 +0700

    Create SafeMath.sol

---
+++ contracts/contracts/utils/SafeMath.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= type(uint256).max - a, "SafeMath: addition overflow");
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        require(b <= type(uint256).max / a, "SafeMath: multiplication overflow");
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+}
