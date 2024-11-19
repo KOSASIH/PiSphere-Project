e7c1959aff27f270cc4922cfb7f99229651c318a
KOSASIH <kosasihg88@gmail.com>
2024-11-19 22:25:04 +0700

    Create Staking.js

---
+++ src/client/pages/Staking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Staking = () => {
+    return (
+        <div>
+            <h1>Staking</h1>
+            <p>Stake your PiTokens to earn rewards.</p>
+        </div>
+    );
+};
+
+export default Staking;
