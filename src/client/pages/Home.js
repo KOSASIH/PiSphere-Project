ff1d81c5ed5719d2d55d40bba08baf8cbb597675
KOSASIH <kosasihg88@gmail.com>
2024-11-19 22:22:04 +0700

    Create Home.js

---
+++ src/client/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            < h1>Welcome to PiSphere</h1>
+            <p>Your decentralized platform for proposals and staking.</p>
+        </div>
+    );
+};
+
+export default Home;
