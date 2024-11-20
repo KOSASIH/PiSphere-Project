946d268b242e544a6a6276b47e6c451346867e4a
KOSASIH <kosasihg88@gmail.com>
2024-11-20 07:43:43 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,35 @@
-const DAO = artifacts.require("DAO");
-const MultiSigWallet = artifacts.require("MultiSigWallet");
+// scripts/deploy.js
 
-module.exports = async function (deployer) {
-    await deployer.deploy(MultiSigWallet, [/* array of owner addresses */], 2); // 2 of 3 signers required
-    await deployer.deploy(DAO);
-    console.log("Multi-Signature Wallet deployed!");
-    console.log("DAO contract deployed!");
-};
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    // Deploy Governance Contract
+    const Governance = await ethers.getContractFactory("Governance");
+    const governance = await upgrades.deployProxy(Governance, { initializer: 'initialize' });
+    await governance.deployed();
+    console.log("Governance Contract deployed to:", governance.address);
+
+    // Deploy Token Contract
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy("MyToken", "MTK", ethers.utils.parseEther("1000000"));
+    await token.deployed();
+    console.log("Token Contract deployed to:", token.address);
+
+    // Deploy Marketplace Contract
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Marketplace.deploy();
+    await marketplace.deployed();
+    console.log("Marketplace Contract deployed to:", marketplace.address);
+}
+
+// Execute the deployment script
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
