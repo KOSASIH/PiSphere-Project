df325af8f4ead949db81a712d9435bb573919a95
KOSASIH <kosasihg88@gmail.com>
2024-11-21 12:10:34 +0700

    Create CrossChainBridge.java

---
+++ src/main/java/com/pisphere/crosschain/CrossChainBridge.java
@@ -0,0 +1,7 @@
+package com.pisphere.crosschain;
+
+public class CrossChainBridge {
+    public void transfer(String asset, double amount, String targetChain) {
+        // Logic to transfer asset System.out.println("Transferring " + amount + " of " + asset + " to " + targetChain);
+    }
+}
