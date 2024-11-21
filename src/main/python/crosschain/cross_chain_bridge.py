c3c9f359c154a3d0fac27cf0d832ed874de71c63
KOSASIH <kosasihg88@gmail.com>
2024-11-21 12:53:59 +0700

    Create cross_chain_bridge.py

---
+++ src/main/python/crosschain/cross_chain_bridge.py
@@ -0,0 +1,4 @@
+class CrossChainBridge:
+    def transfer(self, asset, amount, target_chain):
+        # Logic to transfer asset
+        print(f"Transferring {amount} of {asset} to {target_chain}")
