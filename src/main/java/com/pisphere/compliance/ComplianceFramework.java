3bb8b820edf7f54fcef2ac7a1a4889585d9cd0c6
KOSASIH <kosasihg88@gmail.com>
2024-11-21 12:18:55 +0700

    Create ComplianceFramework.java

---
+++ src/main/java/com/pisphere/compliance/ComplianceFramework.java
@@ -0,0 +1,8 @@
+package com.pisphere.compliance;
+
+public class ComplianceFramework {
+    public void checkCompliance(String transactionDetails) {
+        // Logic to check compliance
+        System.out.println("Checking compliance for transaction: " + transactionDetails);
+    }
+}
